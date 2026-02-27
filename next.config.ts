02b9b4cff14962cd3bd58a2feea878d498afa318
Girones Gerard <Gerard.Girones@freixenet.com>
2026-02-27 17:27:18 +0100

    Update next.config.ts for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap/0.xml",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
